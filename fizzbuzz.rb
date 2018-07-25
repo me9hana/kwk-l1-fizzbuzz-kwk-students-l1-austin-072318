81e28b69c31422bfead741cad05eaa6740ae58fa
me9hana <meghanagoli@gmail.com>
2018-07-25 18:04:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def number(num)
 if num % 5 == 0 && num % 3 == 0
-  puts "FizzBuzz"
+  return "FizzBuzz"
 elsif num % 3 == 0
-  puts "Fizz"
+  return "Fizz"
 elsif num % 5 == 0
-  puts "Buzz"
+  return "Buzz"
 else
   nil
   end
